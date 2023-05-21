0666374c650668b816e0527a93e55d1ff20f8cc1
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-05-21 08:59:14 -0300

    [fix] error in path

--- internal/domain/usecases/publish.go
+++ internal/domain/usecases/publish.go
@@ -10,7 +10,7 @@ import (
 )
 
 func Publish(conn net.Conn, message entity.Message, topic, path string) error {
-	filePath := fmt.Sprintf("%s/%s", path, topic)
+	filePath := fmt.Sprintf("%s/%s.topic", path, topic)
 
 	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
