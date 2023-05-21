0b78b94cadd37b9f50fdc6b63d7efce97b1c98ea
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-05-21 14:40:28 -0300

    [refactor] method to reduce cognitive complexity

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -11,24 +11,11 @@ import (
 )
 
 func main() {
-	host := os.Getenv("HOST")
-	if host == "" {
-		host = "localhost"
-	}
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "9001"
-	}
+	host := getEnv("HOST", "localhost")
+	port := getEnv("PORT", "9001")
 
 	fmt.Printf("connecting on %s:%s\n", host, port)
-	tcpAddr, err := net.ResolveTCPAddr("tcp", host+":"+port)
-	if err != nil {
-		println("ResolveTCPAddr failed:", err.Error())
-		os.Exit(1)
-	}
-
-	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	conn, err := net.Dial("tcp", host+":"+port)
 	if err != nil {
 		println("Dial failed:", err.Error())
 		os.Exit(2)
@@ -38,52 +25,69 @@ func main() {
 	flagPublisher := flag.Bool("p", false, "use of publisher")
 	topic := flag.String("t", "", "topic to consume")
 	consumerName := flag.String("n", "", "consumer name")
-
 	message := flag.String("m", "", "message to publish")
 	flag.Parse()
 
-	if !*flagConsumer && !*flagPublisher {
+	handleFlags(*flagConsumer, *flagPublisher, *topic, *consumerName, *message, conn)
+}
+
+func getEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
+}
+
+func handleFlags(isConsumer, isPublisher bool, topic, consumerName, message string, conn net.Conn) {
+	if !isConsumer && !isPublisher {
 		println("Must specify either consumer or publisher flag")
 		os.Exit(3)
 	}
 
-	if *flagConsumer && *flagPublisher {
+	if isConsumer && isPublisher {
 		println("Cannot specify both consumer or publisher flags")
 		os.Exit(4)
 	}
 
-	if *topic == "" {
+	if topic == "" {
 		println("Must specify the topic")
 		os.Exit(5)
 	}
 
-	switch true {
-	case *flagConsumer:
-		if consumerName == nil || *consumerName == "" {
-			println("Must specify the consumer name to publish")
-			os.Exit(6)
-		}
+	if isConsumer {
+		handleConsumer(consumerName, topic, conn)
+	} else if isPublisher {
+		handlePublisher(message, topic, conn)
+	}
+}
 
-		messages, err := client.Consume(conn, *topic, *consumerName)
-		if err != nil {
-			println(err)
-			os.Exit(7)
-		}
-
-		for m := range messages {
-			b, _ := json.Marshal(m)
-			println(string(b))
-		}
-	case *flagPublisher:
-		if message == nil || *message == "" {
-			println("Must specify the message to publish")
-			os.Exit(8)
-		}
-
-		if err := client.Publish(conn, *message, *topic); err != nil {
-			println(err)
-			os.Exit(9)
-		}
+func handleConsumer(consumerName, topic string, conn net.Conn) {
+	if consumerName == "" {
+		println("Must specify the consumer name to publish")
+		os.Exit(6)
 	}
 
+	messages, err := client.Consume(conn, topic, consumerName)
+	if err != nil {
+		println(err)
+		os.Exit(7)
+	}
+
+	for m := range messages {
+		b, _ := json.Marshal(m)
+		println(string(b))
+	}
+}
+
+func handlePublisher(message, topic string, conn net.Conn) {
+	if message == "" {
+		println("Must specify the message to publish")
+		os.Exit(8)
+	}
+
+	if err := client.Publish(conn, message, topic); err != nil {
+		println(err)
+		os.Exit(9)
+	}
 }
